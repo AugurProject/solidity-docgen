bcdb74fa846f8528ee821f06ccf71eb24f29fac1
adrake33 <aarondrake@gmail.com>
2019-06-05 11:01:05 -0700

    More tweaks

--- src/gather/solidity/compile.js
+++ src/gather/solidity/compile.js
@@ -2,8 +2,9 @@ import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 import glob from 'glob';
-import solc from 'solc';
+import { spawn } from 'child_process';
 import { promisify } from 'util';
+import { join } from 'path';
 
 const globAsync = promisify(glob);
 const readFileAsync = promisify(fs.readFile);
@@ -23,29 +24,138 @@ const compilerSettings = {
   },
 };
 
-export async function compile(directory, ignore) {
-  const files = await globAsync(path.join(directory, '**/*.sol'), {
-    ignore: ignore.map(i => path.join(i, '**/*')),
-  });
+const readdirP = promisify(fs.readdir)
+const statP = promisify(fs.stat)
+async function asyncFilter(arr, filterFunction) {
+  const fail = Symbol()
+  return (await Promise.all(arr.map(async item => (await filterFunction(item)) ? item : fail))).filter(i=>i!==fail)
+}
 
-  const sources = _.fromPairs(await Promise.all(files.map(async file => [
-    file,
-    { content: await readFileAsync(file, 'utf8') },
-  ])));
+async function recursiveReadDir(dir, ignore, allFiles = []) {
+    let files = (await readdirP(dir)).map(f => join(dir, f));
+    allFiles.push(...files);
+    await Promise.all(
+        files.map(
+            async f => (await statP(f)).isDirectory() && recursiveReadDir(f, ignore, allFiles)
+        )
+    )
+    return await asyncFilter(allFiles, async (file) => {
+        const stat = await statP(file);
+        return stat.isFile() && !ignore(file, stat);
+    });
+}
 
-  const inputJSON = {
+async function generateCompilerInput(directory, configuration) {
+  const ignoreFile = function(file, stats) {
+    if (['IAugur', 'IAuction', 'IAuctionToken', 'IDisputeOverloadToken', 'IDisputeCrowdsourcer', 'IDisputeWindow', 'IUniverse', 'IMarket', 'IReportingParticipant', 'IReputationToken', 'IOrders', 'IShareToken', 'Order', 'IV2ReputationToken', 'IInitialReporter'].includes(path.parse(file).base.replace(".sol", ""))) return true;
+    return stats.isFile() && path.extname(file) !== ".sol";
+  }
+  const filePaths = await recursiveReadDir(directory, ignoreFile);
+  let filesPromises;
+  if (configuration.useFlattener) {
+    filesPromises = filePaths.map(async filePath => (await this.generateFlattenedSolidity(filePath)));
+  } else {
+    filesPromises = filePaths.map(async filePath => (await readFileAsync(filePath)).toString('utf8'));
+  }
+  const files = await Promise.all(filesPromises);
+
+  let inputJson = {
     language: "Solidity",
-    sources: sources,
-    settings: compilerSettings,
+    settings: {
+      remappings: [ `ROOT=${directory}/`],
+      optimizer: {
+        enabled: true,
+        runs: 500
+      },
+      outputSelection: {
+        "*": {
+          "": [ "ast" ],
+          "*": [ "abi", "devdoc", "userdoc", "evm.bytecode.object", "evm.methodIdentifiers" ]
+        }
+      }
+    },
+    sources: {}
   };
-
-  const solcOutputString = solc.compile(JSON.stringify(inputJSON));
-  const solcOutput = JSON.parse(solcOutputString);
-
-  if (_.some(solcOutput.errors, ['severity', 'error'])) {
-    console.error(solcOutput.errors);
-    throw new Error();
+  if (configuration.enableSdb) {
+      inputJson.settings.optimizer = {
+          enabled: false
+      }
+      inputJson.settings.outputSelection["*"][""] = [ "legacyAST" ];
+      inputJson.settings.outputSelection["*"]["*"].push("evm.bytecode.sourceMap");
+      inputJson.settings.outputSelection["*"]["*"].push("evm.deployedBytecode.object");
+      inputJson.settings.outputSelection["*"]["*"].push("evm.deployedBytecode.sourceMap");
+      inputJson.settings.outputSelection["*"]["*"].push("evm.methodIdentifiers");
+  }
+  for (var file in files) {
+      const filePath = filePaths[file].replace(directory, "").replace(/\\/g, "/").replace(/^\//, "");;
+      inputJson.sources[filePath] = { content : files[file] };
   }
 
-  return solcOutput;
+  return inputJson;
+}
+
+async function getCommandOutputFromInput(childProcess, stdin) {
+  return ((resolve, reject) => {
+    if (childProcess.stdout === null || childProcess.stdin === null || childProcess.stderr == null) {
+      throw Error("ChildProcess fields stdin, stdout, and stderr must not be null.");
+    }
+
+    const buffers = [];
+    childProcess.stdout.on('data', function (data) {
+      buffers.push(data);
+    });
+    const errorBuffers = [];
+    childProcess.stderr.on('data', function (data) {
+      errorBuffers.push(data);
+    });
+    childProcess.on('close', function (code) {
+      const errorMessage = Buffer.concat(errorBuffers).toString();
+      if (code > 0) return reject(new Error(`Process Exit Code ${code}\n${errorMessage}`))
+      return resolve(Buffer.concat(buffers).toString());
+    });
+    childProcess.stdin.write(stdin);
+    childProcess.stdin.end();
+  })
+}
+
+// TODO: Use solcjs compileStandardWrapper when it works, 0.5.4 giving error: "Runtime.functionPointers[index] is not a function"
+async function compileCustomWrapper(compilerInputJson) {
+  const childProcess = spawn("solc", ["--standard-json"]);
+  const compilerOutputJson = await getCommandOutputFromInput(childProcess, JSON.stringify(compilerInputJson));
+  return JSON.parse(compilerOutputJson);
+}
+
+export async function compile(directory, ignore) {
+  const configuration = {
+    enableSdb: false,
+    useFlattener: false,
+  };
+  const compilerInputJson = await generateCompilerInput(directory, configuration);
+  const compilerOutput = await compileCustomWrapper(compilerInputJson);
+  return compilerOutput;
+
+  // const files = await globAsync(path.join(directory, '**/*.sol'), {
+  //   ignore: ignore.map(i => path.join(i, '**/*')),
+  // });
+
+  // const sources = _.fromPairs(await Promise.all(files.map(async file => [
+  //   file,
+  //   { content: await readFileAsync(file, 'utf8') },
+  // ])));
+
+  // const inputJSON = {
+  //   language: "Solidity",
+  //   sources: sources,
+  //   settings: compilerSettings,
+  // };
+
+  // const solcOutputString = solc.compile(JSON.stringify(inputJSON));
+  // const solcOutput = JSON.parse(solcOutputString);
+
+  // if (_.some(solcOutput.errors, ['severity', 'error'])) {
+  //   console.error(solcOutput.errors);
+  //   throw new Error();
+  // }
+
+  // return solcOutput;
 }
--- src/gather/solidity/index.js
+++ src/gather/solidity/index.js
@@ -1,7 +1,19 @@
 import { readFile } from 'async-file';
+// import { compile } from './compile';
 import { extractDocsPerDirectory } from './extract';
 
 export async function gatherSolidityDocs(solcOutputFile, directory, ignore) {
+  // This code has been modified to use a solc output file as input rather  
+  // than compiling a specified directory. This was done as a workaround for
+  // the sake of expediency, since attempting to compile the existing Augur 
+  // directory structure was causing errors.
+
+  // Much of the code from ContractCompiler.ts has been copied to compile.js,
+  // but is not working in its current state.
+
+  // const solcOutput = await compile(directory, ignore);
+  // return extractDocsPerDirectory(solcOutput, directory);
+  
   const solcOutput = JSON.parse(await readFile(solcOutputFile, "utf8"));
   return extractDocsPerDirectory(solcOutput, "");
 }
